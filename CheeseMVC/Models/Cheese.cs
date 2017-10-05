4ced650c84a33a12c8273433fd1a2d20e5f28f59
Antquanique <antquaniquedancy@gmail.com>
2017-10-05 13:40:35 -0400

    Added a ViewModel

--- CheeseMVC/Models/Cheese.cs
+++ CheeseMVC/Models/Cheese.cs
@@ -9,16 +9,20 @@ namespace CheeseMVC.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public CheeseType Type { get; set; }
         public int CheeseId { get; set; }
         private static int nextId = 1;
 
-
-        public Cheese() {
+      public Cheese()
+        {
             CheeseId = nextId;
             nextId++;
-        }
-       
 
-    }
+
+        }
+
+
+
+}
 }
     
